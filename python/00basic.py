5298157e8e3ef07202b85cd7cf9dba8de22b67e0
wini <e7418741877@gmail.com>
2025-07-28 11:03:35 +0900

    update

---
+++ python/00basic.py
@@ -0,0 +1,25 @@
+# "Hello World 출력하기"
+# print("Hello World")
+
+# number = 10
+# double = 2*number
+# print(double)
+# number = 5
+# print(double)
+# double = 2*number
+# print(double)
+
+# num = 3.14
+# print(num)
+# num = 314e-2
+# print(num)
+
+# my_str = 'hello'
+# print(my_str[4::-1])
+# print(my_str[:4:-1])
+# print(my_str[-4:-2:-2]) # -4부터-2면 진행방향이 오른쪽 근데 -2만큼 가라했으니까 안나옴
+# print(my_str[-4:-2:2])
+
+# my_str = 'hello'
+# my_str[1] = 'a'  # 문자열은 변경 불가
+# print(my_str)  # TypeError 발생
